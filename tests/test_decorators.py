1cfa2401e059520191e517a911082a28d7946d24
sayreks <v.bezgubovv1@gmail.com>
2023-11-13 18:10:06 +0500

    DZ_5 fix

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,11 +1,27 @@
+import os.path
+import datetime
+import pytest
+
 from src.decorators import log
 
 
-@log(filename="test_log.txt")
-def add(x, y):
-    return x + y
+@pytest.mark.parametrize("arg_1, arg_2, expexted_result", [(1, 0, " foo error: ZeroDivisionError. Inputs: (1, 0), {}"),
+                                                           (1, 2, " foo ok")])
+def test_log_decorator_1(arg_1, arg_2, expexted_result):
+    filename = 'test.txt'
+    if os.path.exists(filename):
+        os.remove(filename)
 
+    @log(filename)
+    def foo(x: int, y: int) -> float:
+        return x / y
 
-def test_log_decorator_1():
-    assert add(3, 5) == 8
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    foo(arg_1, arg_2)
 
+    with open(filename) as file:
+        log_mess = file.read().strip()
+
+    expected_log = now + expexted_result
+
+    assert log_mess == expected_log
